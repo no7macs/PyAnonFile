6b0475cf061f15917fb2b933f102ef2ee652c265
no7mac <44577325+no7macs@users.noreply.github.com>
2022-02-03 08:30:50 -0700

    removed aids class and made everything simpler

--- pyanonfile.py
+++ pyanonfile.py
@@ -1,34 +1,23 @@
 import requests
 import urllib
+    
+def upload(self, file, service = 'anonfile'):
+    server_list = {'anonfile': 'https://api.anonfiles.com',
+                    'openload': 'https://api.openload.cc',
+                    'letsupload': 'https://api.letsupload.cc',
+                    'megaupload': 'https://api.megaupload.nz',
+                    'bayfiles': 'https://api.bayfiles.com'}
+    imagefile = {'file': open(file, 'rb')}
+    response = requests.post(server_list[service] + '/upload', files=imagefile).json()
+    if (response)['status'] == False: return(None)
+    return(response)
 
-class upload:
-    def __init__(self, file, service = 'anonfile'):
-        self.file = file
-        self.service = service
-        server_list = {'anonfile': 'https://api.anonfiles.com',
-                        'openload': 'https://api.openload.cc',
-                        'letsupload': 'https://api.letsupload.cc',
-                        'megaupload': 'https://api.megaupload.nz',
-                        'bayfiles': 'https://api.bayfiles.com'}
+def getLink(response, encode=False):
+    filelink = (response['data']['file']['url']['full'])
+    if encode == True:
+        filelink = urllib.parse.quote_plus(filelink)
+    return(filelink)
 
-        imagefile = {'file': open(self.file, 'rb')}
-
-        response = requests.post(server_list[self.service] + '/upload', files=imagefile)
-        self.response = response.json()
-
-        self.status = ((self.response)['status'])
-        if self.status == False: pass
-
-        return
-
-    def getLink(self, encode=False):
-        self.filelink = (self.response['data']['file']['url']['full'])
-        if encode == True:
-            self.filelink = urllib.parse.quote_plus(self.filelink)
-        return(self.filelink)
-
-    def getRaw(self):
-        return(self.response)
-
-    def getStatus(self):
-        return(self.status)
+def getStatus(response):
+    if response == None:
+        return((response)['status'])
